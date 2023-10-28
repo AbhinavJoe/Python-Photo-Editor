6d78db1b740b240c2810ea414e0fe23f13e38106
AbhinavJoe <abhinavjoshi102003@gmail.com>
2023-10-28 15:23:12 +0530

    Transform Images

--- photoeditor.py
+++ photoeditor.py
@@ -1,20 +1,22 @@
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageColor
 import os
 
-path = "./imgs"
-pathOut = "./editedImgs"
+pathIn = './imgs'
+pathOut = './editedImgs'
 
-for filename in os.listdir(path):
-    img = Image.open(f"{path}/{filename}")
+for filename in os.listdir(pathIn):
+    img = Image.open(f"{pathIn}/{filename}")
 
-    # sharpening, BW
-    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(0)
+    # sharpen, BW, transform, rotate
+    # Image tranform size set for sample device and image. Can be changed according to image and device.
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').transform(
+        (1366, 768), Image.AFFINE, (1, 0, 0, 0, 1, 0)).rotate(0)
 
     # contrast
-    factor = 1.5
+    factor = 2
     enhancer = ImageEnhance.Contrast(edit)
     edit = enhancer.enhance(factor)
 
     clean_name = os.path.splitext(filename)[0]
 
-    edit.save(f'{pathOut}/{clean_name}_edited.jpg')
+    edit.save(f"{pathOut}/{clean_name}_edited.jpg")
